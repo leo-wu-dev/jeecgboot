eaf351cac7433796eba591bafe91d886f27d20db
leowu <leo.wu.email@gmail.com>
2025-05-12 19:37:00 +0800

    update

---
+++ jeecg-boot/jeecg-module-system/jeecg-system-start/deploy.sh
@@ -0,0 +1,15 @@
+appName="jeecg-system-start"
+docker build -t "$appName" .
+# 判断容器是否存在
+container_id=$(docker ps -a --filter "name=${appName}" -q)
+if [ -n "$container_id" ]; then
+  # 容器存在，停止并删除
+  echo "Stopping container ${appName}..."
+  docker stop ${appName}
+  echo "Removing container ${appName}..."
+  docker rm ${appName}
+else
+  echo "Container ${appName} does not exist."
+fi
+docker run --name "$appName" -d --restart always -p 8009:8088 "$appName"
+docker image prune -f
